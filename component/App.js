5edd3cd9043682f8d469e1d1f68186cafc9be90b
ching <ndchinh2003@gmail.com>
2022-09-15 07:50:03 +0700

    commiting

--- component/App.js
+++ component/App.js
@@ -1,25 +1,10 @@
-<<<<<<< HEAD
 import html from '../core.js'
 import { connect } from '../store.js'
 
-function App({ cars }) {
+function App() {
     return html`
         <h1>HELLO</h1>
     `
 }
 
 export default connect()(App)
-=======
-// html
-import html from '../core.js'
-
-function App() {
-    return html`
-    <h1>HELLO WORLD</h1>
-    `
-}
-
-export default App
-
-// checked
->>>>>>> d2392a0 (commit on self)
